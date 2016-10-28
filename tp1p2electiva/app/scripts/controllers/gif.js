9cced4499346dbb8ca8d48eba415d90f8c5e564b
Rafaelest <rafaer93@gmail.com>
2016-10-28 15:24:55 -0300

    modules/debug/node_modules/ms/README.md Primer tp 2do parcial

---
+++ tp1p2electiva/app/scripts/controllers/gif.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsAgilApp.controller:GifCtrl
+ * @description
+ * # GifCtrl
+ * Controller of the angularjsAgilApp
+ */
+angular.module('angularjsAgilApp')
+  .controller('GifCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
